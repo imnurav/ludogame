1e592e4bdc9132c47ffa35f5be371cdc37c8a347
Varun Kumar <nurav0402@gmail.com>
2021-10-27 12:59:10 +0530

    issue fix

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 // console.log("ludo game");
 
-$(document).ready(function(){
-    $('button').on("click",()=>{
-
-        const player1=Math.floor(Math.random()*6)+1
-        const player2=Math.floor(Math.random()*6)+1
-        const player1dice='dice'+player1+'.png'
-        const player2dice="dice"+player2+".png"
-        $("#check1").attr("src",player1dice)
-        $("#check2").attr("src",player2dice)
-        if(player1>player2){
-            $("#change").text("PLAYER 1 WON :)")
-        } else if(player1<player2){
-            $("#change").text("PLAYER 2 WON :)")
-        }
-        else{
-            $("#change").text("DRAW")
-
-        }
-    })
-})
+$(document).ready(function () {
+  $("button").on("click", () => {
+    const player1 = Math.floor(Math.random() * 6) + 1;
+    const player2 = Math.floor(Math.random() * 6) + 1;
+    const player1dice = "/dice" + player1 + ".png";
+    const player2dice = "/dice" + player2 + ".png";
+    $("#check1").attr("src", player1dice);
+    $("#check2").attr("src", player2dice);
+    if (player1 > player2) {
+      $("#change").text("PLAYER 1 WON :)");
+    } else if (player1 < player2) {
+      $("#change").text("PLAYER 2 WON :)");
+    } else {
+      $("#change").text("DRAW");
+    }
+  });
+});
